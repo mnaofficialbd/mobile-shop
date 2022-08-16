df4912b33260524ec7c8d61fa7b31912568081ed
mnaofficial <mna.official92@gmail.com>
2022-08-16 23:31:07 +0600

    shopping cart table setup

--- pages/cart.js
+++ pages/cart.js
@@ -14,15 +14,37 @@ const CartScreen = () => {
         <Layout title='Shopping Cart'>
             <h1 className="mb-4 text-xl">Shopping Cart</h1>
             {
-                cartItems.length ===0 ? 
-                (<div>
-                    Cart is Empty. <Link href='/'>Go Shopping</Link>
-                </div>) :
-                (
-                    <div className="grid md:grid-cols-4 md:gap-5">
-                        
-                    </div>
-                )
+                cartItems.length === 0 ?
+                    (<div>
+                        Cart is Empty. <Link href='/'>Go Shopping</Link>
+                    </div>) :
+                    (
+                        <div className="grid md:grid-cols-4 md:gap-5">
+                            <div className="overflow-x-auto md:col-span-3">
+                                <table className='min-w-full'>
+                                    <thead className="border-b">
+                                        <tr>
+                                            <th className="px-5 text-left">Item</th>
+                                            <th className="p-5 text-right">Quantity</th>
+                                            <th className="p-5 text-right">Price</th>
+                                            <th className="p-5">Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {cartItems.map((item) => (
+                                            <tr key={item.slug} className="border-b">
+                                                <td>
+                                                    <Link href={`/product/${item.slug}`}>
+                                                        
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    )
             }
         </Layout>
     );
