c8b0aa9f05ced9230a285353fe9ca2cf37c84370
mnaofficial <mna.official92@gmail.com>
2022-08-16 22:22:01 +0600

    cart item show function

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,10 +1,14 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
 const Layout = ({ title, children }) => {
     const today = new Date();
     const year = today.getFullYear();
+    const { state, dispatch } = useContext(Store);
+    const { cart } = state;
+
     return (
         <>
             <Head>
@@ -21,7 +25,13 @@ const Layout = ({ title, children }) => {
                         <div>
                             <Link href='/'><a className='p-2'>Home</a></Link>
                             <Link href='/about'><a className='p-2'>About</a></Link>
-                            <Link href='/cart'><a className='p-2'>Cart</a></Link>
+                            <Link href='/cart'><a className='p-2'>Cart
+                            {cart.cartItems.length > 0 && (
+                                <span className='ml-1 rounded-full bg-red-700 py-1 px-2 font-bold text-white text-xs'>
+                                    {cart.cartItems.reduce((a,c)=>a+c.quantity, 0)}
+                                </span>
+                            )}
+                            </a></Link>
                             <Link href='/login'><a className='p-2'>Login</a></Link>
                         </div>
                     </nav>
@@ -30,7 +40,7 @@ const Layout = ({ title, children }) => {
                     {children}
                 </main>
                 <footer className='flex justify-center items-center h-10 shadow-inner'>
-                <p className=''>Copyright © {year} || All Rights Reserved by Mobile Shop</p>
+                    <p className=''>Copyright © {year} || All Rights Reserved by Mobile Shop</p>
                 </footer>
             </div>
         </>
